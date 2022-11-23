f5ef9cce242fd470a81bb43bd5669a44116b7c23
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-11-23 09:20:38 +0300

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+    validate :clickbait_y
+
+  TITLES = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+
+  def clickbait_y
+    if TITLES.none? { |t| t.match title }
+      errors.add(:title, "Title should be clickbait_y")
+    end
+  end
+
 end
